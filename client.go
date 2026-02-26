d1b7f422d4a7aaa7aa5e471fae2473b7bb4cdaa2
Kevin Burke <kevin@burke.dev>
2026-02-26 11:49:10 -0800

    1.6.0

--- client.go
+++ client.go
@@ -1127,7 +1127,7 @@ func (c *Client) MakeRequest(ctx context.Context, method string, pathPart string
 	return c.Do(req, &v)
 }
 
-const Version = "1.5.0"
+const Version = "1.6.0"
 
 var defaultUserAgent = "ynab-go/" + Version
 
